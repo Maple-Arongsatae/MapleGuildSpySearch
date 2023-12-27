f2d54a0c28d2f788d0efb1905cc9908bb30cf34c
tseop <77163842+seop-kim@users.noreply.github.com>
2023-12-28 03:29:06 +0900

    AppConfig 변수명 수정

--- src/main/java/com/maple/api/util/AppConfig.java
+++ src/main/java/com/maple/api/util/AppConfig.java
@@ -5,15 +5,15 @@ import java.io.InputStream;
 import java.util.Properties;
 
 public class AppConfig {
-    private static final Properties pro = new Properties();
+    private static final Properties PRO = new Properties();
 
     public static String getApiKeyProperty(String key) {
-        InputStream in = AppConfig.class.getClassLoader().getResourceAsStream("config/jdbc.properties");
+        InputStream in = AppConfig.class.getClassLoader().getResourceAsStream("config/apikey.properties");
         try {
-            pro.load(in);
+            PRO.load(in);
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return pro.getProperty(key);
+        return PRO.getProperty(key);
     }
 }
