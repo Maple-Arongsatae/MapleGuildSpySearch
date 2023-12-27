a4d528218b9c8f7c5a74365a18d52a00e6e2d03a
tseop <77163842+seop-kim@users.noreply.github.com>
2023-12-28 03:28:27 +0900

    Test 데이터 생성

---
+++ src/main/java/com/maple/member/service/MemberTestService.java
@@ -0,0 +1,23 @@
+package com.maple.member.service;
+
+import com.maple.member.model.Member;
+import com.maple.member.repository.MemberTestRepository;
+import com.maple.member.util.dto.MemberDto;
+import java.util.List;
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MemberTestService {
+    private final MemberTestRepository memberRepository;
+
+    public void save(Member member) {
+        memberRepository.save(member);
+    }
+
+    public Map<String, List<MemberDto>> getMembers() {
+        return memberRepository.getMembers();
+    }
+}
