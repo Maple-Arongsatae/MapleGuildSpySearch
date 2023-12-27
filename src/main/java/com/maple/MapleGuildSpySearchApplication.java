59568c5b44564b47cd292c5eec9ac1aa4e241c02
tseop <77163842+seop-kim@users.noreply.github.com>
2023-12-27 22:18:15 +0900

    init

---
+++ src/main/java/com/maple/MapleGuildSpySearchApplication.java
@@ -0,0 +1,13 @@
+package com.maple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapleGuildSpySearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapleGuildSpySearchApplication.class, args);
+	}
+
+}
