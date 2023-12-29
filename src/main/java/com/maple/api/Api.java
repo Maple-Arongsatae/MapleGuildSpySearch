b6d100b688081e9c30b2678a0c6ad5f74d4dec47
tseop <77163842+seop-kim@users.noreply.github.com>
2023-12-30 05:42:05 +0900

    Api 호출 리팩토링

--- src/main/java/com/maple/api/Api.java
+++ src/main/java/com/maple/api/Api.java
@@ -1,45 +1,48 @@
 package com.maple.api;
 
 import com.maple.api.util.ApiConfig;
+import com.maple.global.exception.advice.CustomException;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import lombok.Getter;
 
 public class Api {
+    @Getter
     private static Long count = 0L;
-    public String getRequest(String urlString) throws IOException {
-        count++;
-        String API_KEY = ApiConfig.getApiKeyProperty("key");
-        URL url = new URL(urlString);
+    public String getRequest(String urlString) throws CustomException {
+        try {
+            count++;
+            String API_KEY = ApiConfig.getApiKeyProperty("key");
+            URL url = new URL(urlString);
 
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        connection.setRequestMethod("GET");
-        connection.setRequestProperty("x-nxopen-api-key", API_KEY);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setRequestProperty("x-nxopen-api-key", API_KEY);
 
-        int responseCode = connection.getResponseCode();
+            int responseCode = connection.getResponseCode();
 
-        BufferedReader in;
-        if (responseCode == 200) {
-            in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            BufferedReader in;
+            if (responseCode == 200) {
+                in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
 
-        } else {
-            in = new BufferedReader(new InputStreamReader(connection.getErrorStream()));
+            } else {
+                in = new BufferedReader(new InputStreamReader(connection.getErrorStream()));
+            }
+
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+            return response.toString();
+        } catch (IOException e) {
+            throw new CustomException(e);
         }
-
-        String inputLine;
-        StringBuffer response = new StringBuffer();
-
-        while ((inputLine = in.readLine()) != null) {
-            response.append(inputLine);
-        }
-        in.close();
-
-        return response.toString();
-    }
-
-    public static Long getCount() {
-        return count;
     }
 }
