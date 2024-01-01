07aa19e9f07f6ce59c5073c06b403da46bdbd4c4
tseop <77163842+seop-kim@users.noreply.github.com>
2024-01-01 23:54:48 +0900

    중복 길드 검증 함수 수정

--- src/main/java/com/maple/home/util/validate/ListDuplicateValidator.java
+++ src/main/java/com/maple/home/util/validate/ListDuplicateValidator.java
@@ -2,13 +2,18 @@ package com.maple.home.util.validate;
 
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Set;
 
 public class ListDuplicateValidator {
     // 길드 중복값 제거
     public static List<String> removeDuplicates(List<String> list) {
-        Set<String> set = new HashSet<>(list);
-        return new ArrayList<>(set);
+        LinkedHashMap<String, String> set = new LinkedHashMap<>();
+        for (String guildName : list) {
+            set.put(guildName, guildName);
+        }
+
+        return new ArrayList<>(set.values());
     }
 }
