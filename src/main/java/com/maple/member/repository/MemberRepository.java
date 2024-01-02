a33a126033fb74fed458fb43bf98e9462a54962c
tseop <77163842+seop-kim@users.noreply.github.com>
2024-01-02 21:10:40 +0900

    repository scope 적용

--- src/main/java/com/maple/member/repository/MemberRepository.java
+++ src/main/java/com/maple/member/repository/MemberRepository.java
@@ -1,14 +1,18 @@
 package com.maple.member.repository;
 
 import com.maple.member.model.Member;
+import jakarta.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@Scope(value = "prototype", proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class MemberRepository {
 
     private Map<String, List<Member>> allGuilds;
@@ -40,6 +44,7 @@ public class MemberRepository {
         return allGuilds;
     }
 
+    @PostConstruct
     public void createObj() {
         allGuilds = new HashMap<>();
         allNicknames = new ArrayList<>();
